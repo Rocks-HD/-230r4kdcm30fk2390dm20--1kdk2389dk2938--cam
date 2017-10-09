3b13ae5e105ac4d41f8161962034f8141af147e3
99346826134 <brunodacostamonteiro@gmail.com>
2017-10-09 16:19:34 -0300

    Melhorias gerais

--- www/js/modules/mobileapp/controllers/OpiniaoController.js
+++ www/js/modules/mobileapp/controllers/OpiniaoController.js
@@ -36,12 +36,12 @@ angular.module('camara')
                         console.log(detalhes);
                         var html = '';
                         for (var i in detalhes['dados']) {
+                            console.log(detalhes['dados'][i]);
                             html =  '<div class="row linha-zebrada text-center padding-vertical item'+ detalhes['dados'][i]['id'] +'" data-id="'+ detalhes['dados'][i]['id'] +'">'+
                                         '<div class="col-xs-3">'+
                                             '<h3>'+
-                                                '<a href="'+detalhes['dados'][i]['uri']+'" target="_blank">'+ lstCodProposicao[detalhes['dados'][i]['id']] +'</a>'+
+                                                lstCodProposicao[detalhes['dados'][i]['id']]+
                                             '</h3>'+
-                                            '<img src="./js/modules/mobileapp/layouts/imagens/icones/ico-mao-clique.svg" alt="clique e saiba mais" class="posicionamento-mao"/>'+
                                         '</div>'+
                                         '<div class="col-xs-6"><h6>'+ detalhes['dados'][i]['ementa'] +'</h6></div>'+
                                         '<div class="col-xs-3">'+
